b45911ec0492f7f67037f3bdb8c2192551b2cb20
RicardoMachadoOFC <cado.machado@hotmail.com>
2024-08-26 13:07:47 -0300

    organização de código

--- Main.java
+++ Main.java
@@ -5,7 +5,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-
         String[] nomes = leiaNomes();
         double[] precoCusto = leiaCusto(nomes);
         int[] quantidadeInicialEstoque = leiaQuantidade(nomes);
@@ -30,26 +29,22 @@ public class Main {
                 System.out.println("Opção 2 selecionada");
                 break;
 
-
         }
 
     }
 
     public static String[] leiaNomes() {
 
-
         String[] nomes = new String[10];
         for (int i = 0; i < nomes.length; i++) {
             System.out.println("digite o nome do produto " + (1 + i) + " ");
             nomes[i] = input.nextLine();
-
 
         }
         return nomes;
     }
 
     public static double[] leiaCusto(String[] nomes) {
-
 
         double[] precoCusto = new double[10];
         for (int i = 0; i < 10; i++) {
@@ -58,12 +53,11 @@ public class Main {
         }
         return precoCusto;
 
-
     }
 
-    public static int[] leiaQuantidade( String[]nomes){
-        int [] quantidadeInicialEstoque = new int[nomes.length];
-        for (int i = 0; i < nomes.length; i++){
+    public static int[] leiaQuantidade(String[] nomes) {
+        int[] quantidadeInicialEstoque = new int[nomes.length];
+        for (int i = 0; i < nomes.length; i++) {
             System.out.println("qual a quantidade incial do p roduto: " + nomes[i]);
             quantidadeInicialEstoque[i] = Integer.parseInt(input.nextLine());
         }
@@ -71,14 +65,14 @@ public class Main {
     }
 
     public static String leiaOpcoes() {
-        System.out.println("digite 1 para 'Venda de produto', 2 para 'Reposição de estoque', 3 para 'Relatório de Estoque', 4 para 'Relatório de vendas' ou 5 para Sair.");
+        System.out.println(
+                "digite 1 para 'Venda de produto', 2 para 'Reposição de estoque', 3 para 'Relatório de Estoque', 4 para 'Relatório de vendas' ou 5 para Sair.");
         return input.nextLine();
     }
 
     public static String pergunteProdutos(String[] nomes) {
         boolean encontrado;
         String nomeProduto = " ";
-
 
         do {
             System.out.println("digite o nome do produto a ser vendido: ");
@@ -91,19 +85,14 @@ public class Main {
                     nomeProduto = nomes[i];
                     break;
 
-
-
                 }
             }
             if (!encontrado) {
                 System.out.println("produto não encontrado na lista, tente novamente");
             }
 
-
         } while (!encontrado);
-
 
         return nomeProduto;
     }
 }
-
